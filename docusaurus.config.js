bc60346735333afb6171f70330080a9d49e4acf8
Jason Christopher <jason.christopher@u.nus.edu>
2023-05-28 19:45:24 +0800

    Add Classroom 360 Config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         title: 'Classroom360',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/pink-unicorn.png',
         },
         items: [
           {
